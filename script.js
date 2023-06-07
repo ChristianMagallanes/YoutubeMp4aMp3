8c1c42b49b73cd199f9e43bed03ac111ed4a1423
christian magallanes <dar4del-x@hotmail.com>
2023-06-07 01:33:23 -0300

    primer commit

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-const spinnerWrapperEl = document.querySelector('.spinner-wrapper');
-
 setTimeout(() => {
-  spinnerWrapperEl.style.opacity = '0';
-}, 500);
+    const spinnerWrapperEl = document.querySelector('.spinner-wrapper');
+    spinnerWrapperEl.style.display = 'none';
+  }, 1000);
+  document.getElementById('convert').addEventListener('click', function() {
+    const spinnerWrapperEl = document.createElement('div');
+    spinnerWrapperEl.className = 'spinner-wrapper';
+    
+    const spinnerEl = document.createElement('div');
+    spinnerEl.className = 'spinner-border text-dark';
+    spinnerEl.setAttribute('role', 'status');
+    
+    const spinnerTextEl = document.createElement('span');
+    spinnerTextEl.className = 'visually-hidden';
+    spinnerTextEl.textContent = 'Loading...';
+    
+    spinnerEl.appendChild(spinnerTextEl);
+    spinnerWrapperEl.appendChild(spinnerEl);
+    
+    document.body.appendChild(spinnerWrapperEl);
+    
+    setTimeout(() => {
+      spinnerWrapperEl.remove();
+    }, 1000);
+  });
+  
